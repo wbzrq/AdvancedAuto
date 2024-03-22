f899e349313bb3adadd66a4c3080eec9472483bd
Вова Наронский <naronskiy2006@gmail.com>
2024-03-22 19:02:07 +0300

    Добавьте файлы проекта.

---
+++ AdvancedAuto/AuthWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdvancedAuto
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtLogin.Focus();
+        }
+
+        private void txtLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtLogin.Text) && txtLogin.Text.Length > 0)
+            {
+                textLogin.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textLogin.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPass_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPass.Focus();
+        }
+
+        private void txtPass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPass.Password) && txtPass.Password.Length > 0)
+            {
+                textPass.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPass.Visibility = Visibility.Visible;
+            }
+        }
+
+        bool isPasswordShow = false;
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isPasswordShow)
+            {
+                imgLock.Source = new BitmapImage(new Uri("/Images/lock_close_icon.png", UriKind.Relative));
+            }
+            else
+            {
+                imgLock.Source = new BitmapImage(new Uri("/Images/lock_open_icon.png", UriKind.Relative));
+            }
+            isPasswordShow = !isPasswordShow;
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            grdLogIn.Visibility = Visibility.Hidden;
+        }
+    }
+}
